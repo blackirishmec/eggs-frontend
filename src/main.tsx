6cb35a5e83138676a321f7d09de52d1668dd98b1
Michael Coughlin <blackirishmec@gmail.com>
2025-03-31 11:35:23 -0400

    feat: add Redux Toolkit and React-Redux for state management; update axios instance creation

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
+import { createAxiosInstance } from './api/axiosInstance';
 import App from './App';
+
+export const axiosInstance = createAxiosInstance();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
