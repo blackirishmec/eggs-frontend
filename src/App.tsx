0fae4b666e4981a82b62534562fa20a4cef003a6
Michael Coughlin <blackirishmec@gmail.com>
2025-03-31 11:13:07 -0400

    feat: add Poppins font styles and update layout structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { Col, Row } from './components/layout/FlexComponents';
 
 function App() {
 	return (
-		<Row className="flex-1">
-			<Col className="flex-1">
+		<Row className="flex-1 poppins-regular">
+			<Col className="flex-1" childrenHorizontalPosition="center">
 				<Row>
-					<div>Header</div>
+					<h1>
+						On <s>y</s>
+						<u>Our</u> Face
+					</h1>
 				</Row>
-				<Row className="flex-1"></Row>
+				<Row className="flex-1">
+					<Col className="flex-1">
+						<Row className="flex-1">Number</Row>
+						<Row className="flex-1">Chart</Row>
+					</Col>
+				</Row>
 				<Row>Footer</Row>
 			</Col>
 		</Row>
